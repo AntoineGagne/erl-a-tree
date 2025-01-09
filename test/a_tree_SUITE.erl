73f17680b934b01838368572396cc199cecc9f43
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2025-01-09 15:09:00 -0500

    Fix formatting

--- test/a_tree_SUITE.erl
+++ test/a_tree_SUITE.erl
@@ -6,56 +6,66 @@
 -compile(nowarn_export_all).
 -compile(export_all).
 
--define(SOME_ATTRIBUTES,
-        [{boolean, <<"private">>},
-         {string, <<"country">>},
-         {integer, <<"exchange_id">>},
-         {string, <<"city">>},
-         {string_list, <<"deals">>},
-         {integer_list, <<"segment_ids">>}]).
+-define(SOME_ATTRIBUTES, [
+    {boolean, <<"private">>},
+    {string, <<"country">>},
+    {integer, <<"exchange_id">>},
+    {string, <<"city">>},
+    {string_list, <<"deals">>},
+    {integer_list, <<"segment_ids">>}
+]).
 -define(AN_ID, 1).
--define(AN_EXPRESSION,
-        <<"exchange_id = 1 and not private and deals one of [\"deal-1\", "
-          "\"deal-2\"]">>).
+-define(AN_EXPRESSION, <<
+    "exchange_id = 1 and not private and deals one of [\"deal-1\", "
+    "\"deal-2\"]"
+>>).
 -define(AN_INVALID_EXPRESSION, <<"exchange_id = 1 and deals one of [\"deal-1\"">>).
 -define(AN_EXPRESSION_WITH_A_NON_EXISTING_ATTRIBUTE, <<"a_string = \"test\"">>).
--define(SOME_VALID_EXPRESSIONS_AND_IDS,
-        [{?AN_ID,
-          <<"exchange_id = 1 and not private and deals one of [\"deal-1\", "
-            "\"deal-2\"]">>},
-         {2,
-          <<"exchange_id = 1 and not private and deals one of [\"deal-2\", "
-            "\"deal-3\"]">>},
-         {3,
-          <<"exchange_id = 1 and not private and deals one of [\"deal-2\", "
-            "\"deal-3\"] and segment_ids one of [1, 2, 3, 4]">>},
-         {4,
-          <<"exchange_id = 1 and not private and deals one of [\"deal-2\", "
-            "\"deal-3\"] and segment_ids one of [5, 6, 7, 8] and country "
-            "in [\"CA\", \"US\"]">>}]).
--define(AN_EVENT,
-        [{<<"private">>, false},
-         {<<"exchange_id">>, 1},
-         {<<"deals">>, [<<"deal-1">>, <<"deal-3">>]},
-         {<<"segment_ids">>, [2, 3]},
-         {<<"country">>, <<"CA">>}]).
+-define(SOME_VALID_EXPRESSIONS_AND_IDS, [
+    {?AN_ID, <<
+        "exchange_id = 1 and not private and deals one of [\"deal-1\", "
+        "\"deal-2\"]"
+    >>},
+    {2, <<
+        "exchange_id = 1 and not private and deals one of [\"deal-2\", "
+        "\"deal-3\"]"
+    >>},
+    {3, <<
+        "exchange_id = 1 and not private and deals one of [\"deal-2\", "
+        "\"deal-3\"] and segment_ids one of [1, 2, 3, 4]"
+    >>},
+    {4, <<
+        "exchange_id = 1 and not private and deals one of [\"deal-2\", "
+        "\"deal-3\"] and segment_ids one of [5, 6, 7, 8] and country "
+        "in [\"CA\", \"US\"]"
+    >>}
+]).
+-define(AN_EVENT, [
+    {<<"private">>, false},
+    {<<"exchange_id">>, 1},
+    {<<"deals">>, [<<"deal-1">>, <<"deal-3">>]},
+    {<<"segment_ids">>, [2, 3]},
+    {<<"country">>, <<"CA">>}
+]).
 
 all() ->
     [{group, creation}, {group, insertion}, {group, search}, {group, deletion}].
 
 groups() ->
-    [{creation,
-      [parallel],
-      [can_create_an_empty_tree,
-       can_create_a_tree,
-       return_an_error_when_creating_a_tree_with_duplicate_attributes]},
-     {insertion,
-      [parallel],
-      [can_insert_a_valid_expression, return_an_error_when_inserting_an_invalid_expression]},
-     {search, [parallel], [can_search_a_tree]},
-     {deletion,
-      [parallel],
-      [can_delete_an_expression_from_a_tree, deleting_a_non_existing_expression_ignores_it]}].
+    [
+        {creation, [parallel], [
+            can_create_an_empty_tree,
+            can_create_a_tree,
+            return_an_error_when_creating_a_tree_with_duplicate_attributes
+        ]},
+        {insertion, [parallel], [
+            can_insert_a_valid_expression, return_an_error_when_inserting_an_invalid_expression
+        ]},
+        {search, [parallel], [can_search_a_tree]},
+        {deletion, [parallel], [
+            can_delete_an_expression_from_a_tree, deleting_a_non_existing_expression_ignores_it
+        ]}
+    ].
 
 init_per_group(_Group, Config) ->
     Config.
@@ -88,8 +98,10 @@ can_create_a_tree(_Config) ->
     ?assertMatch({ok, _}, a_tree:new([{boolean, <<"private">>}])).
 
 return_an_error_when_creating_a_tree_with_duplicate_attributes(_Config) ->
-    ?assertMatch({error, _},
-                 a_tree:new([{boolean, <<"private">>}, {integer, <<"private">>}])).
+    ?assertMatch(
+        {error, _},
+        a_tree:new([{boolean, <<"private">>}, {integer, <<"private">>}])
+    ).
 
 can_insert_a_valid_expression(Config) ->
     Tree = proplists:get_value(tree, Config),
@@ -104,13 +116,17 @@ return_an_error_when_inserting_an_invalid_expression(Config) ->
 return_an_error_when_inserting_an_expression_that_refers_to_a_non_existing_attribute(Config) ->
     Tree = proplists:get_value(tree, Config),
 
-    ?assertMatch({error, _},
-                 a_tree:insert(Tree, ?AN_ID, ?AN_EXPRESSION_WITH_A_NON_EXISTING_ATTRIBUTE)).
+    ?assertMatch(
+        {error, _},
+        a_tree:insert(Tree, ?AN_ID, ?AN_EXPRESSION_WITH_A_NON_EXISTING_ATTRIBUTE)
+    ).
 
 can_search_a_tree(Config) ->
     Tree = proplists:get_value(tree, Config),
-    lists:foreach(fun({Id, Expression}) -> ok = a_tree:insert(Tree, Id, Expression) end,
-                  ?SOME_VALID_EXPRESSIONS_AND_IDS),
+    lists:foreach(
+        fun({Id, Expression}) -> ok = a_tree:insert(Tree, Id, Expression) end,
+        ?SOME_VALID_EXPRESSIONS_AND_IDS
+    ),
 
     Results = a_tree:search(Tree, ?AN_EVENT),
 
@@ -120,8 +136,10 @@ can_search_a_tree(Config) ->
 
 can_delete_an_expression_from_a_tree(Config) ->
     Tree = proplists:get_value(tree, Config),
-    lists:foreach(fun({Id, Expression}) -> ok = a_tree:insert(Tree, Id, Expression) end,
-                  ?SOME_VALID_EXPRESSIONS_AND_IDS),
+    lists:foreach(
+        fun({Id, Expression}) -> ok = a_tree:insert(Tree, Id, Expression) end,
+        ?SOME_VALID_EXPRESSIONS_AND_IDS
+    ),
 
     Results = a_tree:search(Tree, ?AN_EVENT),
     ?assertMatch({ok, _}, Results),
